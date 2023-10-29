7f75397cf30f5db54a4ec5f0f7b2e192957d847b
sssabaa <61809355+sssabaa@users.noreply.github.com>
2023-10-28 20:56:45 -0400

    Add files via upload

---
+++ homework6/app/app.js
@@ -0,0 +1,31 @@
+$(".hamburger-icon").on("click", () => {
+    $(".hamburger-icon").toggleClass("open");
+
+});
+
+function changeRoute() {
+let hashTag = window.location.hash;
+let pageID = hashTag.replace('#', '');
+
+if (pageID != '') {
+$.get(`pages/${pageID}.html`, function (data) {
+console.log('data ' + data);
+$('#app').html(data);
+});
+}
+else {
+$.get(`pages/home.html`, function (data) {
+$('#app').html(data);
+});
+
+}
+}
+
+function initURLListener() {
+$(window).on('hashchange', changeRoute);
+changeRoute();
+}
+
+$(document).ready(function () {
+initURLListener();
+});
